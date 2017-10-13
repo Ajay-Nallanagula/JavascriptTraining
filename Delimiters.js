0caaa4ee5ddbd8718dfc44b8a540b02f8e27544a
Ajay-Nallanagula <ajay_nallanagula@epam.com>
2017-10-13 15:36:33 +0530

    Changed after running the test cases

--- Delimiters.js
+++ Delimiters.js
@@ -1,3 +1,4 @@
 ﻿//http://www.codewars.com/kata/printing-array-elements-with-comma-delimiters
-var input = ["h", "o", "l", "a"];
-console.log(input.toString());
+function printArray(array){
+  return array.toString();
+}

83bd4b3eeeac52c40f4545d2bb9f192670f38828
Ajay-Nallanagula <ajay_nallanagula@epam.com>
2017-10-13 15:34:34 +0530

    Changed the logic after running test cases

--- CarRental.js
+++ CarRental.js
@@ -1,15 +1,15 @@
 ﻿
 //http://www.codewars.com/kata/transportation-on-vacation
-var carRental = function(vacationDays){
-var rent;
+function rentalCarCost(vacationDays) {
+  var rent;
 if(vacationDays >= 7){
-rent = (vacationDays*7*40) - 50;
+rent = (vacationDays*40) - 50;
 }else if(vacationDays >= 3){
-rent = (vacationDays*3*40) - 20;
+rent = (vacationDays*40) - 20;
 }else{
 rent = vacationDays*40;
 }
-return "$"+rent;
+return rent;//"$"+rent;
 }
 
 console.log(carRental(2));

68777f69e458de3566bd317b431c77c274740ee5
Ajay-Nallanagula <ajay_nallanagula@epam.com>
2017-10-20 13:50:38 +0530

    Code Review Comments
     Is there any other way to do it ? instead of array.toString()
    Yes there are many other ways to solve this i choose the laziest and easiest one :P,
    Another alternate way is added

--- Delimiters.js
+++ Delimiters.js
@@ -1,4 +1,20 @@
 ﻿//http://www.codewars.com/kata/printing-array-elements-with-comma-delimiters
-function printArray(array){
-  return array.toString();
+//Way 2:
+//http://www.codewars.com/kata/printing-array-elements-with-comma-delimiters
+function printArray(array) {
+    var str = '';
+    for (var i = 0; i < array.length; i++) {
+        str = str + array[i];
+        if (i !== array.length - 1) {
+            str = str + ',';
+        }
+    }
+    return str;
 }
+
+/*
+//Way 1:
+function printArray(array) {
+    return array.toString();
+}
+*/

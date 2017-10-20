214d752cff68214bb74bfd2ba532bc373bfdee30
Ajay-Nallanagula <ajay_nallanagula@epam.com>
2017-10-20 13:59:11 +0530

    Code Review Comments
    Muquthar:  Don’t declare variable which are not used by other scopes. Ex :- result  instead return it.
    Ajay : Fixed it

--- OppositeNumbers.js
+++ OppositeNumbers.js
@@ -2,6 +2,5 @@
 //http://www.codewars.com/kata/opposite-number
 function opposite(numb) {
 var errMsg = "Recheck your input, please type a number";
-var result = isNaN(numb) ? errMsg : (parseInt(numb) === 0 ? 0 : -numb);
-return result;
+return isNaN(numb) ? errMsg : (parseInt(numb) === 0 ? 0 : -numb);
 }

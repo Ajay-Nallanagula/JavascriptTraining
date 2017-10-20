0885390bc76f3400d4d98ddb576f1b11968faae3
Ajay-Nallanagula <ajay_nallanagula@epam.com>
2017-10-20 13:54:36 +0530

    Code review comments
     Muquthar : This case is failing due to ternary operation which is not required.  Just use val1-val2 and execute the task in code wars and update the same in git tasks.
    Ajay :  I think the either test cases or Instructions  in code wars must be updated, Its very much valid to have subtraction done from greater value,let me know your thoughts on this

--- MathOps.js
+++ MathOps.js
@@ -1,35 +1,38 @@
-﻿//http://www.codewars.com/kata/basic-mathematical-operations
-var basicOp = function (opr, val1, val2) {
+﻿ //http://www.codewars.com/kata/basic-mathematical-operations
+var basicOp = function(opr, val1, val2) {
     var result;
 
     switch (opr) {
 
-        case '+': {
-            result = val1 + val2;
-            break;
-        }
+        case '+':
+            {
+                result = val1 + val2;
+                break;
+            }
 
-        case '-': {
-            result = val1 >= val2 ? val1 - val2 : val2 - val1;
-            break;
-        }
+        case '-':
+            {
+                result = val1 >= val2 ? val1 - val2 : val2 - val1;
+                break;
+            }
 
-        case '*': {
-            result = val1 * val2;
-            break;
-        }
+        case '*':
+            {
+                result = val1 * val2;
+                break;
+            }
 
-        case '/': {
-            result = val1 / val2;
-            break;
-        }
+        case '/':
+            {
+                result = val1 / val2;
+                break;
+            }
 
-        default: {
-            result = "Enter only basic Operation, check operation is valid";
-        }
+        default:
+            {
+                result = "Enter only basic Operation, check operation is valid";
+            }
 
     }
     return result;
 }
-
-console.log(basicOp('-', 10, 20));

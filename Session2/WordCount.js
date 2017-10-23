83deba837487a42dcb355e6663f447b700cba10d
Ajay-Nallanagula <ajay_nallanagula@epam.com>
2017-10-23 12:33:23 +0530

    Updated the code

--- Session2/WordCount.js
+++ Session2/WordCount.js
@@ -1,16 +1,21 @@
 //http://www.codewars.com/kata/word-count
 function countWords(str) {
-    var regex = /[^\u0000-\u00ff]/; //To test Unicode characters
+    var length;
+    var breakWords;
+    var regex;
+
+    regex = /[^\u0000-\u00ff]/; //To test Unicode characters
     if (regex.test(str)) {
         str = str.replace(/(?! )\s/g, ' '); //To replace unicode characters
     }
-    var breakWords = str.trim().split(" ");
-    var length = 0;
+
+    breakWords = str.trim().split(" ");
+    length = 0;
+
     for (var i = 0; i < breakWords.length; i++) {
         if (breakWords[i].length > 0) {
             length += 1;
         }
     }
-
     return length;
 }

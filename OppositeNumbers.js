591fb37417c9ba01f53b1a03a447e6b825870c11
Ajay-Nallanagula <ajay_nallanagula@epam.com>
2017-10-23 12:12:06 +0530

    Indentation Fixed

--- OppositeNumbers.js
+++ OppositeNumbers.js
@@ -1,6 +1,6 @@
 ﻿
 //http://www.codewars.com/kata/opposite-number
 function opposite(numb) {
-var errMsg = "Recheck your input, please type a number";
-return isNaN(numb) ? errMsg : (parseInt(numb) === 0 ? 0 : -numb);
+    var errMsg = "Recheck your input, please type a number";
+    return isNaN(numb) ? errMsg : (parseInt(numb) === 0 ? 0 : -numb);
 }

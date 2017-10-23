37fd4f1a398da2749969866d9b448308d2da3c2f
Ajay-Nallanagula <ajay_nallanagula@epam.com>
2017-10-23 12:25:18 +0530

    Indentation done

--- Session2/GetMiddle.js
+++ Session2/GetMiddle.js
@@ -1,7 +1,5 @@
 //http://www.codewars.com/kata/get-the-middle-character
-function getMiddle(s)
-{
+function getMiddle(s) {
     var strLen = s.length;
-    isOdd = strLen%2 > 0;
-    return isOdd ? s.substr(strLen/2,1) : s.substr((strLen/2-1),2);
+    return strLen % 2 > 0 ? s.substr(strLen / 2, 1) : s.substr((strLen / 2 - 1), 2);
 }

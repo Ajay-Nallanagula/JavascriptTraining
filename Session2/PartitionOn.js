9f6dba1040b38c4ef1b904224927c4b4cca5e2ac
Ajay-Nallanagula <ajay_nallanagula@epam.com>
2017-10-26 13:18:17 +0530

    Fixed review comment
    1.	Use single var pattern. Declare one Var for all the variables.
    2.	Use forEach instead of for loop

--- Session2/PartitionOn.js
+++ Session2/PartitionOn.js
@@ -1,25 +1,29 @@
 //http://www.codewars.com/kata/partition-on
  function partitionOn(pred, items) {
-     var trueArr = [];
-     var falseArr = [];
-     var index = -1;
-     var pt;
-     items.forEach(function(item) {
+    var trueArr, falseArr, index, resultArry;
 
-         if (pred(item)) {
-             trueArr.push(item);
-         } else {
-             falseArr.push(item);
-         }
-     });
+    trueArr = [];
+    falseArr = [];
+    index = -1;
 
-     pt = falseArr.concat(trueArr);
-     for (var i = 0; i < pt.length; i++) {
-         if (index < 0 && pred(pt[i])) {
-             index = i;
-         }
-         items[i] = pt[i];
-     }
-     return index;
+    items.forEach(function(item) {
 
- }
+        if (pred(item)) {
+            trueArr.push(item);
+        } else {
+            falseArr.push(item);
+        }
+    });
+
+    resultArry = falseArr.concat(trueArr);
+
+    resultArry.forEach(function(elem, counter) {
+        if (index < 0 && pred(elem)) {
+            index = counter;
+        }
+        items[counter] = elem;
+    });
+
+    return index;
+
+}

9c1c33f275abedf31312e814f373162c34719249
Ajay-Nallanagula <ajay_nallanagula@epam.com>
2017-11-02 13:35:09 +0530

    Implemented Logic with closures

--- Session2/Calculating with Functions.js
+++ Session2/Calculating with Functions.js
@@ -1,4 +1,74 @@
-function zero(param) {
+function calc(num, calculate) {
+    return !calculate ? num : calculate(num);
+}
+
+function zero(operation) {
+    return calc(0, operation);
+}
+
+function one(operation) {
+    return calc(1, operation);
+}
+
+function two(operation) {
+    return calc(2, operation);
+}
+
+function three(operation) {
+    return calc(3, operation);
+}
+
+function four(operation) {
+    return calc(4, operation);
+}
+
+function five(operation) {
+    return calc(5, operation);
+}
+
+function six(operation) {
+    return calc(6, operation);
+}
+
+function seven(operation) {
+    return calc(7, operation);
+}
+
+function eight(operation) {
+    return calc(8, operation);
+}
+
+function nine(operation) {
+    return calc(9, operation);
+}
+
+function times(a) {
+    return function(b) {
+        return a * b;
+    }
+}
+
+function plus(a) {
+    return function(b) {
+        return a + b;
+    }
+}
+
+function minus(a) {
+    return function(b) {
+        return b - a;
+    }
+}
+
+function dividedBy(a) {
+    return function(b) {
+        return b / a;
+    }
+}
+
+
+
+/*function zero(param) {
     return findDigitOrSymbol(zero.name, param);
 }
 
@@ -162,3 +232,5 @@ function getValue(val1) {
 	
     return result;
 }
+
+*/

99c4fcbf0085055d7afb2405233b3d4f6b006045
Ajay-Nallanagula <ajay_nallanagula@epam.com>
2017-11-02 13:03:02 +0530

    Update PartitionOn.js

--- Session2/PartitionOn.js
+++ Session2/PartitionOn.js
@@ -1,11 +1,9 @@
 //http://www.codewars.com/kata/partition-on
- function partitionOn(pred, items) {
+function partitionOn(pred, items) {
     var trueArr, falseArr, index, resultArry;
-
     trueArr = [];
     falseArr = [];
     index = -1;
-
     items.forEach(function(item) {
 
         if (pred(item)) {
@@ -14,16 +12,12 @@
             falseArr.push(item);
         }
     });
-
     resultArry = falseArr.concat(trueArr);
-
     resultArry.forEach(function(elem, counter) {
         if (index < 0 && pred(elem)) {
             index = counter;
         }
         items[counter] = elem;
     });
-
     return index;
-
 }

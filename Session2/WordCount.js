a369d89c129db143fd697ed96281f43e9746a971
Ajay-Nallanagula <ajay_nallanagula@epam.com>
2017-11-02 13:04:22 +0530

    Updated Review Comments

--- Session2/WordCount.js
+++ Session2/WordCount.js
@@ -1,12 +1,10 @@
 //http://www.codewars.com/kata/word-count
 function countWords(str) {
     var length, breakWords, regex, breakWords;
-    
     regex = /[^\u0000-\u00ff]/; //To test Unicode characters
     if (regex.test(str)) {
         str = str.replace(/(?! )\s/g, ' '); //To replace unicode characters
     }
-    
     breakWords = str.trim().match(/\S+/g) || [];
     return breakWords.length;
 }
